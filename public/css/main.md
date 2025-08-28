8c301f235e43a736fb59f4cc0f4a830d589431f0
alyson <alyson.pym@gmail.com>
2025-08-29 09:12:53 +1000

    public

---
+++ public/css/main.md
@@ -0,0 +1,56 @@
+Nav 
+background-color: #19154b;
+
+Footer
+background-color: #191919;
+font-size: 14px
+color: white;
+
+breadcrumbs
+background-color: #cddae5;
+font-size: 100px;
+
+size: 1200 × 350
+
+button:
+background-color: #25c3f3;
+color: #19154b;
+font-size: 16px;
+font-size: 1em;
+
+
+heading
+color: #19164b;
+
+/* about */
+heading
+font-size: 40px;
+
+text
+font-size: 30px;
+
+small text
+font-size: 18px;
+
+
+/* faqs */
+page heading
+font-size: 40px;
+
+heading
+font-size: 28px;
+color: #191919;
+
+text
+font-size: 28px;
+color: #191919;
+
+/* Services */
+buttons
+font-size: 21px;
+
+form
+https://formspree.io/
+
+
+at 600px the Mobile Responsiveness breaks
---
+++ public/js/front.js
@@ -0,0 +1,39 @@
+//Nav Bar menu
+
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('navbar-toggle');
+    const navbarMenu = document.getElementById('navbar-menu');
+  
+    toggleButton.addEventListener('click', function() {
+      navbarMenu.style.display = navbarMenu.style.display === 'block' ? 'none' : 'block';
+    });
+  });
+
+  // Pop-up modal JS
+  document.addEventListener('DOMContentLoaded', function() {
+    // Get the modal
+    var modal = document.getElementById("ptsd-modal");
+  
+    // Get the button that opens the modal (you'll need to add this button to your services page)
+    var btn = document.getElementById("ptsd-button"); // Example button ID
+  
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close-button")[0];
+  
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+  
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+  
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  });
